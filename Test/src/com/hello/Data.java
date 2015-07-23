585d561f0679629c56ce6ca7b8c8a132f6ab453e
aparajitha-n <n_aparajitha@rediffmail.com>
2015-07-23 13:20:48 +0530

    My First Commit

---
+++ Test/src/com/hello/Data.java
@@ -0,0 +1,32 @@
+package com.hello;
+
+public class Data
+{
+private int sid;
+private String sname,address;
+
+public String getAddress()
+{
+	return address;
+}
+public void setAddress(String address)
+{
+	this.address = address;
+}
+public void setSid(int sid)
+{
+	this.sid = sid;
+}
+public int getSid() 
+{
+	return sid;
+}
+public void setSname(String sname)
+{
+	this.sname = sname;
+}
+public String getSname()
+{
+	return sname;
+}
+}
